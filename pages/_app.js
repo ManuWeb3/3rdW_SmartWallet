6d6556a488673ae1dae951341690d87964ed1208
Manu <manu.web3.0@gmail.com>
2023-05-11 18:02:28 -0600

    Added Smart Wallet and gasless txn

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { FACTORY_ADDRESS, API_KEY } from "../const/addresses"
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+      activeChain={activeChain}
+      supportedWallets={[
+        metamaskWallet(),
+        smartWallet({
+          factoryAddress: FACTORY_ADDRESS,
+          thirdwebApiKey: API_KEY,
+          gasless: true,
+          personalWallets: [
+            metamaskWallet(),
+            coinbaseWallet(),
+            localWallet({persist: true})
+          ]
+        })
+      ]}
+    
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,19 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
+import { NFT1155_ADDRESS } from "../const/addresses"
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        <ConnectWallet />
+        <Web3Button
+          contractAddress={NFT1155_ADDRESS}
+          action={(contractInstance) => contractInstance.erc1155.claim(0, 1)}
+          onSuccess={() => alert("NFT Claimed!")}
+        >
+          Claim NFT
+        </Web3Button>
       </main>
     </div>
   );
